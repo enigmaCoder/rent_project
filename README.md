0ea69c79c92e607c788f466f4ccd591ce0c67387
enigmaCoder <62026574+enigmaCoder@users.noreply.github.com>
2025-01-05 12:55:52 +0530

    Hosting Changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# rent_project
- # Rent-Master:  A web application for Owners to manage payments or rents from their tenants
+# rent_master_b0a5b
+"A web application for Owners to manage payments or rents from their tenants"
+
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:rent_project/main.dart';
+import 'package:rent_master_b0a5b/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
